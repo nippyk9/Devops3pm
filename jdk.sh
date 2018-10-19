4ae0c26745b67b3220634e547a2bf3af6e51fb9c
nippyk9 <nippyk9@gmail.com>
2018-10-19 19:53:57 +0530

    Update jdk.sh

--- jdk.sh
+++ jdk.sh
@@ -6,7 +6,9 @@ yum install wget -y
 mkdir /opt/java
 cd /opt/java
 
-wget --no-cookies --no-check-certificate --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jdk-8u181-linux-x64.rpm
-yum localinstall jdk-8u181-linux-x64.rpm -y
+wget --no-cookies --no-check-certificate --header "Cookie: oraclelicense=accept-securebackup-cookie" 
+http://download.oracle.com/otn-pub/java/jdk/8u191-b12/2787e4a523244c269598db4e85c51e0c/jdk-8u191-linux-x64.rpm
+yum localinstall jdk-8u191-linux-x64.rpm -y
+
 
 
